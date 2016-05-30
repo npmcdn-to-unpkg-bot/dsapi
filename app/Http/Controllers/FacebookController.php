2f1339f8a708d11a0037151c8e29a39f09f5bfd1
ntc113 <ngtiencong@gmail.com>
2016-05-30 17:23:17 +0700

    the first commit

---
+++ app/Http/Controllers/FacebookController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use Laravel\Socialite\Facades\Socialite;
+
+class FacebookController extends Controller
+{
+    public function facebook()
+	{
+	    return Socialite::with('facebook')->redirect();
+	}
+
+	public function callback()
+	{
+	    $user = Socialite::with('facebook')->user();
+	    return "<h2>" . $user->getName() . "</h2> <img src='" . $user->getAvatar() . "' />";
+	}
+}
